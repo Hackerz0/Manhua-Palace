19959bbca3f725818f937a199bf0b05827d4c67e
Nexus <88650225+Hackerz0@users.noreply.github.com>
2026-07-15 16:09:28 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-console.log(document.getElementById("header_btn"));
-
 const button = document.getElementById("header_btn");
 const sidebar = document.getElementById("sidebar");
 
+let startX = 0;
+
 button.addEventListener("click", function () {
-  sidebar.classList.toggle("show");
+    sidebar.classList.toggle("show");
+  
+});
+
+sidebar.addEventListener("touchstart", function(event) {
+    startX = event.touches[0].clientX;
+});
+
+sidebar.addEventListener("touchend", function(event) {
+    let endX = event.changedTouches[0].clientX;
+
+    if (startX - endX > 100) {
+        sidebar.classList.remove("show");
+    }
 });
